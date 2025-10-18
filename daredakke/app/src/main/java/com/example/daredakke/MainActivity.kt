b337068a68babfff32aa6e08ab8dbe98871774b8
kdix-23-071 <2312110071r@kindai.ac.jp>
2025-10-19 02:39:49 +0900

    スプラッシュスクリーン設定

--- daredakke/app/src/main/java/com/example/daredakke/MainActivity.kt
+++ daredakke/app/src/main/java/com/example/daredakke/MainActivity.kt
@@ -9,15 +9,18 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.daredakke.ui.theme.daredakkeTheme
 import com.example.daredakke.ui.navigation.AppNavigation
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             daredakkeTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = White,
                 ) {
                     AppNavigation()
                 }
